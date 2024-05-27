b550c8f7456791f8ce43a7fdaf4aafc40d048213
Vishal Hasnani <hasnanivishal@gmail.com>
2024-05-27 17:21:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ const proxy = httpProxy.createProxyServer({});
 const PORT = process.env.PORT || 3000;
 
 proxy.on('error', (err, req, res) => {
+
+  console.log('proxy server error', err);
+  
   res.writeHead(500, {
     'Content-Type': 'text/plain'
   });
-  res.end('<<< Failed to Create httpProxy Server >>>', err);
+  res.end('<<< Failed to Create httpProxy Server >>>');
 });
 
 proxy.on('proxyReq', function(proxyReq, req, res, options) {
@@ -16,8 +19,15 @@ proxy.on('proxyReq', function(proxyReq, req, res, options) {
 });
 
 const server = http.createServer((req, res) => {
-  console.log("Forward the request to ", req.url);
+  console.log("Request URL is", req.url);
+  
+  if(req.url == "/") {
+    console.log("Forward the request Google in case of empty");
+    proxy.web(req, res, { target: '' });
+  }
+
   const targetUrl = req.url;
+  console.log("Forward to targetUrl", targetUrl);
   proxy.web(req, res, { target: targetUrl });
 });
 
